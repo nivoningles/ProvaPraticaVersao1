e1822b0ad4ab5e1618555a1a1ac548bb3112e39e
nivoningles <contadonivon@gmail.com>
2024-10-02 21:08:30 -0300

    Provas

--- src/atividade2.java
+++ src/atividade2.java
@@ -1,70 +1,81 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class atividade2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int quantidadeVotantes = 0;
+        int quantidadeVotantes;
         int votosTaffe = 0;
         int votosRodrigo = 0;
         int votosLucas = 0;
         int votosAlessandro = 0;
-        int[] votos = new int[0];
 
         System.out.print("\n** ELEIÇÃO 2024 COMO MELHOR PROFESSOR **\n");
         System.out.print("\nInformar quantidade de votantes: ");
         quantidadeVotantes = sc.nextInt();
-        votos = new int[quantidadeVotantes];
 
         for (int i = 0; i < quantidadeVotantes; i++) {
-
             System.out.print("\n** AQUI ESTÃO NOSSOS CANDIDATOS **\n");
             System.out.println("99 - Taffe");
             System.out.println("88 - Rodrigo");
             System.out.println("77 - Lucas");
             System.out.println("66 - Alessandro");
-            System.out.println("\nDigite o numero do seu candidato: ");
-            votos[i] = sc.nextInt();
-            while (votos[i] != 66 && votos[i] != 77 && votos[i] != 88 && votos[i] != 99){
-                System.out.print("Número invalido, informe o numero correto do seu candidato: ");
-                votos[i] = sc.nextInt();
-            };
+            System.out.print("\nDigite o número do seu candidato: ");
+            int voto = sc.nextInt();
 
-            if (votos[i] == 66) {
-                votosAlessandro++;
-            } else if (votos[i] == 77) {
-                votosLucas++;
-            } else if (votos[i] == 88) {
-                votosRodrigo++;
-            } else if (votos[i] == 99) {
-                votosTaffe++;
+
+            while (voto != 66 && voto != 77 && voto != 88 && voto != 99) {
+                System.out.print("Número inválido, informe o número correto do seu candidato: ");
+                voto = sc.nextInt();
             }
 
-        }
-        if (votosTaffe > votosRodrigo && votosTaffe > votosAlessandro && votosTaffe > votosLucas) {
-            System.out.println("Taffe foi quem ganhou a eleição");
-        } else if (votosRodrigo > votosTaffe && votosRodrigo > votosLucas && votosRodrigo > votosAlessandro) {
-            System.out.println("Rodrigo foi quem ganhou a eleição");
-        } else if (votosLucas > votosTaffe && votosLucas > votosRodrigo && votosLucas > votosAlessandro) {
-            System.out.println("Lucas foi quem ganhou a eleição");
-        } else if (votosAlessandro > votosTaffe && votosAlessandro > votosLucas && votosAlessandro > votosRodrigo) {
-            System.out.println("Alessandro foi quem ganhou a eleição");
-        } else if (votosTaffe == votosRodrigo && votosTaffe == votosAlessandro && votosTaffe == votosLucas) {
-            System.out.println("Então deu empate de todos os candidatos");
-        } else if (votosTaffe == votosRodrigo && votosTaffe < votosAlessandro && votosAlessandro == votosLucas) {
-            System.out.println("Então deu empate entre Alessandro e Lucas");
-        } else if (votosTaffe == votosRodrigo && votosTaffe > votosAlessandro && votosAlessandro == votosLucas) {
-            System.out.println("Então deu empate entre Taffe e Rodrigo");
-        } else if (votosTaffe == votosAlessandro && votosRodrigo > votosAlessandro && votosRodrigo == votosLucas) {
-            System.out.println("Então deu empate entre Lucas e Rodrigo");
-        } else if (votosTaffe == votosAlessandro && votosRodrigo < votosAlessandro && votosRodrigo == votosLucas) {
-            System.out.println("Então deu empate entre Alessandro e Taffe");
-        } else if (votosTaffe == votosLucas && votosTaffe < votosAlessandro && votosRodrigo == votosAlessandro) {
-            System.out.println("Então deu empate entre Alessandro e Rodrigo");
-        } else if (votosTaffe == votosLucas && votosTaffe > votosAlessandro && votosRodrigo == votosAlessandro) {
-            System.out.println("Então deu empate entre Taffe e Lucas");
 
+            if (voto == 66) {
+                votosAlessandro++;
+            } else if (voto == 77) {
+                votosLucas++;
+            } else if (voto == 88) {
+                votosRodrigo++;
+            } else if (voto == 99) {
+                votosTaffe++;
+            }
+        }
+
+
+        if (votosTaffe > votosRodrigo && votosTaffe > votosAlessandro && votosTaffe > votosLucas) {
+            System.out.println("Taffe foi quem ganhou a eleição.");
+        } else if (votosRodrigo > votosTaffe && votosRodrigo > votosLucas && votosRodrigo > votosAlessandro) {
+            System.out.println("Rodrigo foi quem ganhou a eleição.");
+        } else if (votosLucas > votosTaffe && votosLucas > votosRodrigo && votosLucas > votosAlessandro) {
+            System.out.println("Lucas foi quem ganhou a eleição.");
+        } else if (votosAlessandro > votosTaffe && votosAlessandro > votosLucas && votosAlessandro > votosRodrigo) {
+            System.out.println("Alessandro foi quem ganhou a eleição.");
+        } else {
+
+            if (votosTaffe == votosRodrigo && votosTaffe == votosLucas && votosTaffe > votosAlessandro) {
+                System.out.println("Empate entre Taffe, Rodrigo e Lucas.");
+            } else if (votosTaffe == votosRodrigo && votosTaffe == votosAlessandro && votosTaffe > votosLucas) {
+                System.out.println("Empate entre Taffe, Rodrigo e Alessandro.");
+            } else if (votosTaffe == votosLucas && votosTaffe == votosAlessandro && votosTaffe > votosRodrigo) {
+                System.out.println("Empate entre Taffe, Lucas e Alessandro.");
+            } else if (votosRodrigo == votosLucas && votosRodrigo == votosAlessandro && votosRodrigo > votosTaffe) {
+                System.out.println("Empate entre Rodrigo, Lucas e Alessandro.");
+            } else if (votosTaffe == votosRodrigo && votosTaffe == votosLucas && votosTaffe == votosAlessandro) {
+                System.out.println("Empate geral entre todos os candidatos.");
+            } else {
+
+                if (votosTaffe == votosRodrigo && votosTaffe > votosLucas && votosTaffe > votosAlessandro) {
+                    System.out.println("Empate entre Taffe e Rodrigo.");
+                } else if (votosLucas == votosAlessandro && votosLucas > votosTaffe && votosLucas > votosRodrigo) {
+                    System.out.println("Empate entre Lucas e Alessandro.");
+                } else if (votosTaffe == votosLucas && votosTaffe > votosRodrigo && votosTaffe > votosAlessandro) {
+                    System.out.println("Empate entre Taffe e Lucas.");
+                } else if (votosTaffe == votosAlessandro && votosTaffe > votosRodrigo && votosTaffe > votosLucas) {
+                    System.out.println("Empate entre Taffe e Alessandro.");
+                } else if (votosRodrigo == votosLucas && votosRodrigo > votosTaffe && votosRodrigo > votosAlessandro) {
+                    System.out.println("Empate entre Rodrigo e Lucas.");
+                }
+            }
         }
     }
 }
